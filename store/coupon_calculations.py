e7eb7c3df91879143fa3be85d519caace8fe7680
Michael Harmon <mwharmon1@dmacc.edu>
2019-09-18 21:01:07 -0500

    Added main logic in calculate_order function and added if statement to check for shipping under $10.00. Test set 1 is now passing.

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -6,7 +6,18 @@
 
 
 def calculate_order(price, cash_coupon, percent_coupon):
-    pass
+    TAX = 0.06
+    # shipping rates
+    UP_TO_TEN_DOLLARS = 5.95
+
+    total_after_cash_coupon = price - cash_coupon
+    total_of_percent_off = total_after_cash_coupon * percent_coupon
+    total_after_percent_coupon = total_after_cash_coupon - total_of_percent_off
+    total_to_add_to_tax = total_after_percent_coupon * TAX
+    total_after_tax = total_after_percent_coupon + total_to_add_to_tax
+    if total_after_tax < 10.00:
+        total = total_after_tax + UP_TO_TEN_DOLLARS
+    return round(total, 2)
 
 
 if __name__ == '__main__':
