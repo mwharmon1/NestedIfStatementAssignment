6763364998f4a1be5de28ccfe282f41d41f10135
Michael Harmon <mwharmon1@dmacc.edu>
2019-09-18 21:55:48 -0500

    Added final shipping variable for price over $50, OVER_50_DOLLARS = 0.00 and final else clause to calculate shipping for over $50. All tests are passing.

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -11,6 +11,7 @@ def calculate_order(price, cash_coupon, percent_coupon):
     UP_TO_TEN_DOLLARS = 5.95
     TEN_DOLLARS_AND_UP_TO_THIRTY_DOLLARS = 7.95
     THIRTY_DOLLARS_AND_UP_TO_FIFTY_DOLLARS = 11.95
+    OVER_50_DOLLARS = 0.00
 
     total_after_cash_coupon = price - cash_coupon
     total_of_percent_off = total_after_cash_coupon * percent_coupon
@@ -23,6 +24,8 @@ def calculate_order(price, cash_coupon, percent_coupon):
         total = total_after_tax + TEN_DOLLARS_AND_UP_TO_THIRTY_DOLLARS
     elif 30.00 <= total_after_tax < 50.00:
         total = total_after_tax + THIRTY_DOLLARS_AND_UP_TO_FIFTY_DOLLARS
+    else:
+        total = total_after_tax + OVER_50_DOLLARS
     return round(total, 2)
 
 
